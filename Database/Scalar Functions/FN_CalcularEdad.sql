854218218058d2627f6a3a929aa4c9c210687bb5
naoChiquito <naomi.chiquito@outlook.com>
2026-06-11 22:57:18 -0600

    add scalar functions

---
+++ Database/Scalar Functions/FN_CalcularEdad.sql
@@ -0,0 +1,38 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Scalar Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION dbo.fn_CalcularEdad
+(
+    @BirthDate DATE
+)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @Edad INT;
+
+    SET @Edad = DATEDIFF(YEAR, @BirthDate, GETDATE());
+
+    IF DATEADD(YEAR, @Edad, @BirthDate) > GETDATE()
+        SET @Edad = @Edad - 1;
+
+    RETURN @Edad;
+END;
+GO
+
